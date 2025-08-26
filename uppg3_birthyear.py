bae2270e0bb937c576398d72e7d80859a5bbccb3
benrob080502-creator <benrob080502@student.jenseneducation.se>
2025-08-26 11:31:28 +0200

    done

--- uppg3_birthyear.py
+++ uppg3_birthyear.py
@@ -6,3 +6,13 @@ Exempel på utskrift, det inom () ska ändras om variablerna ändras:
 Du är (16) år gammal.
 
 """
+
+from datetime import datetime
+
+fodelsear = int(input("vilket år är du född? "))
+
+nurvarande_ar = datetime.now().year
+
+alder = nurvarande_ar - fodelsear
+
+print(f"du är ungerfär {alder} år gammal.")
