cfedd698cfd0e82067ffce65453f024f55bfa2d9
benrob080502-creator <benrob080502@student.jenseneducation.se>
2025-08-26 11:19:34 +0200

    done

--- uppg1_utskrift.py
+++ uppg1_utskrift.py
@@ -1,9 +1,15 @@
-"""  
-Skapa en variabel för ditt namn och två variabler för saker du gillar att göra
-Programmet ska sedan skriva ut en mening som använder dessa variabler
+username = "Benjamin"
+print(username)
+password = "12345"
+print(password)
 
-Exempel på utskrift, det inom () ska ändras om variablerna ändras:
-(Hampus) gillar att (kolla på film) och (spela kortspel)
-"""
+username = input("username: ")
+password = input("password: ")
 
+if (username == "Benjamin"):
+    print ("Correct")
 
+if (username == "12345"):
+    print ("Correct")
+
+print(f"välkommen {username}!")
